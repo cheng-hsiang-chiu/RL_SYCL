db941c6eb86672cd297c909658b68a1c2018e5f3
chchiu <chchiu@pass-2.ece.utah.edu>
2022-11-19 13:57:14 -0700

    added num_threads

--- src/policy.hpp
+++ src/policy.hpp
@@ -17,10 +17,15 @@ enum Accelerator {
 class RL_Policy {
 
 public:
-  RL_Policy() = default;
+//  RL_Policy() = default;
+
+  RL_Policy(const size_t n): _num_threads{n} {}
 
   template<typename T>
   std::pair<size_t, Accelerator> policy(T&&);
+
+private:
+  size_t _num_threads;
 };
 
 
@@ -35,7 +40,7 @@ RL_Policy::policy(T&& task) {
   
   static std::minstd_rand engine{std::random_device{}()};
   static std::uniform_int_distribution<size_t> distribution;
-  size_t thread_id = distribution(engine) % 4;
+  size_t thread_id = distribution(engine) % _num_threads;
   return std::make_pair(thread_id, Accelerator::GPU); 
 }
 
